e70b125e30afe39d2fdbc184d16f33e7ec970150
Joaquin Angeles <jqn.ngls@gmail.com>
2024-05-25 00:47:04 -0700

    Added more items to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,32 @@
 import { FC } from "react";
 import { Link, useLocation } from "react-router-dom";
+import Logo from "../Logo/Logo";
+import "./NavBar.css";
 
 const NavBar: FC = () => {
 	const { pathname } = useLocation();
 	return (
 		<nav className="nav-menu">
-			<ul className="nav-tab">
-				<span className="nav-item">Logo</span>
+			<ul className="nav-tab left">
+				<Logo />
 				<li>
 					<Link to={"/"} className={`nav-item ${pathname === "/" ? "active" : null}`}>
 						Home
+					</Link>
+				</li>
+				<li>
+					<Link to={"/"} className={`nav-item ${pathname === "/characters" ? "active" : null}`}>
+						Characters
+					</Link>
+				</li>
+				<li>
+					<Link to={"/"} className={`nav-item ${pathname === "/planets" ? "active" : null}`}>
+						Planets
+					</Link>
+				</li>
+				<li>
+					<Link to={"/"} className={`nav-item ${pathname === "/favorites" ? "active" : null}`}>
+						Favorites
 					</Link>
 				</li>
 				<li>
@@ -18,12 +35,13 @@ const NavBar: FC = () => {
 					</Link>
 				</li>
 			</ul>
-			<ul>
+			<ul className="nav-tab right">
 				<li>
 					<Link to={"/login"} className={`nav-item ${pathname === "/login" ? "active" : null}`}>
 						Login
 					</Link>
 				</li>
+				<span>hey</span>
 			</ul>
 		</nav>
 	);
