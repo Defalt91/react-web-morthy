936f609cb695e9c30a69e5c305eafc4244585710
Joaquin Angeles <jqn.ngls@gmail.com>
2024-05-29 14:22:52 -0700

    Created function to generate random numbers

--- src/customHooks/useRandomData.ts
+++ src/customHooks/useRandomData.ts
@@ -1,10 +1,18 @@
 import { useCallback, useEffect, useState } from "react";
 // import getMortyData from "../apiCalls/getMortydata";
 import { ICharacter } from "../interface";
-const URL_GET_CHARACTERS = import.meta.env.VITE_CHARACTERS_URL;
+const URL_GET_URL = import.meta.env.VITE_GLOBAL_URL;
 
 const useRandomData = (resource: string) => {
 	const [characters, setCharacters] = useState<ICharacter[] | null>(null);
+
+	const generateNumArray = () => {
+		const randomArray = Array(20)
+			.fill("")
+			.map(() => Math.floor(1 + (826 - 1 + 1) * Math.random()));
+
+		return randomArray;
+	};
 
 	const getData = useCallback(async () => {
 		const config = {
@@ -12,26 +20,24 @@ const useRandomData = (resource: string) => {
 			headers: { Accept: "application/json", "Content-Type": "application/json" },
 		};
 
-		console.log("url", URL_GET_CHARACTERS + resource);
+		console.log("url:", `${URL_GET_URL}/${resource}`);
 
 		try {
-			const response = await fetch(URL_GET_CHARACTERS, config);
+			const response = await fetch(`${URL_GET_URL}/${resource}/${generateNumArray()}`, config);
 
 			if (response.status !== 200) {
 				const errorData = await response.json();
-				console.log("getDataApi", errorData);
-				return errorData;
+				console.error("getDataApi", errorData);
+				// return errorData;
 			}
 
 			const data = await response.json();
 
 			// console.log("done data", data.results);
-
-			// return data.results;
-			setCharacters(data.results);
+			setCharacters(data);
 		} catch (err: unknown) {
-			console.log("getDataApi: Check your internet connection!", err);
-			return { errorMessage: "Error: Check your internet connection!" };
+			console.error("getDataApi: Check your internet connection!", err);
+			// return { errorMessage: "Error: Check your internet connection!" };
 		}
 	}, [resource]);
 
@@ -40,7 +46,7 @@ const useRandomData = (resource: string) => {
 		getData();
 	}, [getData]);
 
-	return { characters };
+	return { characters, getData };
 };
 
 export default useRandomData;
